f9e39200f4071d5884cb1716eef15a81e1e3ac0d
Replit user <>
2023-08-10 19:25:17 +0000

    feat: mission complete

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  // console.log(req.ip);
+  // console.log(req.get('Accept-Language'));
+  // console.log(req.get('User-Agent'));
+  res.json({"ipaddress": req.ip, "language": req.get('Accept-Language'), "software": req.get('User-Agent')});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
